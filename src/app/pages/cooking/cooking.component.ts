900b4617191cc18e176ed39eb70f0748569508fa
lydo1797 <lydo1797@gmail.com>
2022-05-02 01:23:44 -0700

    Implement adding new cooking post.

--- src/app/pages/cooking/cooking.component.ts
+++ src/app/pages/cooking/cooking.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddPostDialogComponent } from 'src/app/components/add-post-dialog/add-post-dialog.component';
 import { CookingPost } from 'src/app/models/cooking-post.model';
 import { CookingService } from 'src/app/services/cooking.service';
 
@@ -10,10 +12,29 @@ import { CookingService } from 'src/app/services/cooking.service';
 export class CookingComponent implements OnInit {
   cookingPosts: CookingPost[];
 
-  constructor(private cookingService: CookingService) { }
+
+  constructor(private cookingService: CookingService,
+    private dialog: MatDialog
+    ) { }
 
   async ngOnInit()  {
     this.cookingPosts = await this.cookingService.getCookingPosts();
+  }
+
+  async addNewTravelPost() {
+    const dialogRef = this.dialog.open(AddPostDialogComponent, {
+      data: {
+        title: 'Adding new Travel Post'
+      }
+    });
+
+    const newCookingPost: CookingPost = await dialogRef.afterClosed().toPromise();
+    if (newCookingPost) {
+      console.log('newTravelPost =', newCookingPost);
+      newCookingPost.id = newCookingPost.title.toLowerCase().replace(' ', '-')
+      await this.cookingService.addCookingPost(newCookingPost);
+      this.cookingPosts.push(newCookingPost);
+    }
   }
 
 }
--- src/app/services/cooking.service.ts
+++ src/app/services/cooking.service.ts
@@ -6,27 +6,40 @@ import { CookingPost } from '../models/cooking-post.model';
 })
 export class CookingService {
 
-  constructor() { }
+  constructor() {
+    if (!localStorage.getItem('cookingPosts')) {
+      localStorage.setItem('cookingPosts', JSON.stringify({
+        'egg-roll': {
+          title: 'egg-roll',
+          description: 'Eggroll homemade',
+          imageUrl: 'https://natashaskitchen.com/wp-content/uploads/2020/10/Egg-Rolls-7-728x1092.jpg'
+        }
+      }));
+    }
+  }
 
   getCookingPosts(): Promise<CookingPost[]> {
-    return Promise.resolve([
-      {
-        id: 'egg-roll',
-        title: 'Egg Roll',
-        description: 'Eggroll homemade',
-        imageUrl: 'https://natashaskitchen.com/wp-content/uploads/2020/10/Egg-Rolls-7-728x1092.jpg'
-      },
-    ] as CookingPost[]);
+    const cookingPosts = JSON.parse(localStorage.getItem('cookingPosts'));
+    const res = Object.entries(cookingPosts).reduce((prev, cur) => {
+      prev.push({
+        id: cur[0],
+        ...cur[1] as any
+      })
+      return prev;
+    }, []);
+    return Promise.resolve(res);
   }
 
+  
   getCookingPost(id: string): Promise<CookingPost> {
-    return Promise.resolve(
-      {
-        id: 'egg-roll',
-        title: 'Egg Roll',
-        description: 'Eggroll homemade',
-        imageUrl: 'https://natashaskitchen.com/wp-content/uploads/2020/10/Egg-Rolls-7-728x1092.jpg'
-      }
-    );
+    const cookingPosts = JSON.parse(localStorage.getItem('cookingPosts'));
+    return Promise.resolve(cookingPosts[id]);
   }
-}
+  
+  addCookingPost(cookingPost: CookingPost): Promise<void> {
+    const cookingPosts = JSON.parse(localStorage.getItem('cookingPosts'));
+    cookingPosts[cookingPost.id] = cookingPost;
+    localStorage.setItem('cookingPosts', JSON.stringify(cookingPosts));
+    return Promise.resolve();
+  }
+}  
