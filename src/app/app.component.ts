8e14525609bb172e541a107fbc0723fbabee3b22
Ly Do <lydo1797@gmail.com>
2022-06-16 01:52:10 -0700

    Add swiper infinite loop effect to movie project.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 
 // import Swiper core and required modules
-import SwiperCore, { EffectCube, Pagination } from "swiper";
+import SwiperCore, { EffectCube, Pagination, Navigation } from "swiper";
 
 // install Swiper modules
-SwiperCore.use([EffectCube, Pagination]);
+SwiperCore.use([EffectCube, Pagination, Navigation]);
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  // encapsulation: ViewEncapsulation.None,
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
   title = 'personal-website';
